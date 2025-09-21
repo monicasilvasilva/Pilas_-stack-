0cd359d2d758a9c66db0133cf5cf460bd4948fa8
santiagocanoo <santiagocano922@gmail.com>
2025-09-21 12:56:37 -0500

    implementar menú principal con opciones de edición (Undo/Redo)

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+do {
+        System.out.println("\n== Editor (Undo/Redo con java.util.Stack) ==");
+            System.out.println("1. Escribir texto (agregar línea)");
+            System.out.println("2. Deshacer (Undo)");
+            System.out.println("3. Rehacer (Redo)");
+            System.out.println("4. Mostrar texto actual (peek)");
+            System.out.println("5. Mostrar todo el texto");
+            System.out.println("6. Salir");
+            System.out.print("Elige una opción: ");
